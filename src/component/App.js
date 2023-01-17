c4e4515125450fd53a43fcc8627c4753b8ad1abf
lzb lalala <108671171+funny-lzb@users.noreply.github.com>
2023-01-17 22:30:05 +0800

    Add files via upload

--- src/component/App.js
+++ src/component/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import RecipeList from './RecipeList'
 import RecipeEdit from './RecipeEdit'
 import '../css/app.css'
@@ -21,18 +21,14 @@ function App() {
     handleRecipeChange
   }
 
-  const recipesSaveLocal = useCallback(() => {
-    const recipesJSON = localStorage.getItem(Recipe_LOCAL_STORAGE_KEY)
-
-    if (recipesJSON != null) setRecipes(JSON.parse(recipesJSON))
-  }, [])
-
   useEffect(() => {
     // 因为我们是根据数组渲染UI的，所以一切的源头都是数组。数组要跟本地存储之间形成双向通道
     // 本地存储的意义：就是用本地存储来 “记住”/保存 数组的变化
     // 页面挂载时 ----> 数组更新成上一次的本地存储内容(本地存储--->数组)
-    recipesSaveLocal()
-  }, [recipesSaveLocal])
+    const recipesJSON = localStorage.getItem(Recipe_LOCAL_STORAGE_KEY)
+
+    if (recipesJSON != null) setRecipes(JSON.parse(recipesJSON))
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     // 每次数组改变时 ----> 更新本地存储(数组-->本地存储)
